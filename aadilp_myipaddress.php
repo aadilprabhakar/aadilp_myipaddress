2c6e32431364f0b208820c894c57444edebc2936
Aadil Prabhakar <3303784+aadilprabhakar@users.noreply.github.com>
2022-01-29 10:13:54 +0530

    Update aadilp_myipaddress.php

--- aadilp_myipaddress.php
+++ aadilp_myipaddress.php
@@ -6,5 +6,17 @@
  * Version: 0.1
  * Text Domain: aadilp_myipaddress
  * Author: Aadil Prabhakar
- * Author URI: http://upwork.com/fl/aadilprabhakar
+ * Author URI: 
  */
+
+function aadilp_myipaddress__theip() {
+  if( ! empty( $_SERVER['CF-Connecting-IP'] ) ) {
+    $ip = $_SERVER['CF-Connecting-IP']; //--  --  --  --  --  --  -- Cloudflare support
+  } elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR']; //--  --  --  --  --  -- Any forwarded for IP 
+  } else {
+    $ip = $_SERVER['REMOTE_ADDR'];  //--  --  --  --  --  --  --  -- Nothing else, then its this :D
+  }
+  
+  return $ip;
+}
