f71a794726ca980bb832de8596be6e9da1cad916
Aadil Prabhakar <3303784+aadilprabhakar@users.noreply.github.com>
2022-01-29 10:06:11 +0530

    Create aadilp_myipaddress.php

---
+++ aadilp_myipaddress.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: My IP Address
+ * Plugin URI: 
+ * Description: Enable shortcod to show user/visitor's IP Address
+ * Version: 0.1
+ * Text Domain: aadilp_myipaddress
+ * Author: Aadil Prabhakar
+ * Author URI: http://upwork.com/fl/aadilprabhakar
+ */
