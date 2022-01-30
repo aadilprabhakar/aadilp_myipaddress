3efd6eacb63984feb9d2ee3182607b06ff0d1dad
Aadil Prabhakar <3303784+aadilprabhakar@users.noreply.github.com>
2022-01-30 20:14:14 +0530

    Update aadilp_myipaddress.php

--- aadilp_myipaddress.php
+++ aadilp_myipaddress.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: My IP Address
  * Plugin URI: 
- * Description: Enable shortcod to show user/visitor's IP Address
+ * Description: Enable shortcode to show user/visitor's IP Address
  * Version: 0.1
  * Text Domain: aadilp_myipaddress
  * Author: Aadil Prabhakar
@@ -14,7 +14,7 @@ function aadilp_myipaddress__theip($atts) {
   if( ! empty( $_SERVER['CF-Connecting-IP'] ) ) {
     $ip = $_SERVER['CF-Connecting-IP']; //--  --  --  --  --  --  -- Cloudflare support
   } elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR']; //--  --  --  --  --  -- Any forwarded for IP 
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR']; //--  --  --  --  --  -- Any "forwarded for" IP 
   } else {
     $ip = $_SERVER['REMOTE_ADDR'];  //--  --  --  --  --  --  --  -- Nothing else, then its this :D
   }
